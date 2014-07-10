99945984035acb91ff579ae0020314d39b4fab9b
TW <tanmay.wadhwa@gmail.com>
2014-07-10 16:01:07 -0400

    Added required comments. Final Commit!

--- src/edu/buffalo/cse/cse486586/simplemessenger/SimpleMessengerActivity.java
+++ src/edu/buffalo/cse/cse486586/simplemessenger/SimpleMessengerActivity.java
@@ -45,234 +45,239 @@ import android.widget.TextView;
 //The link to the same is :- http://docs.oracle.com/javase/tutorial/networking/sockets/
 
 public class SimpleMessengerActivity extends Activity {
-	static final String TAG = SimpleMessengerActivity.class.getSimpleName();
-	static final String REMOTE_PORT0 = "11108";
-	static final String REMOTE_PORT1 = "11112";
-	static final int SERVER_PORT = 10000;
+    static final String TAG = SimpleMessengerActivity.class.getSimpleName();
+    static final String REMOTE_PORT0 = "11108";
+    static final String REMOTE_PORT1 = "11112";
+    static final int SERVER_PORT = 10000;
 
-	/** Called when the Activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    /** Called when the Activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		/*
-		 * Allow this Activity to use a layout file that defines what UI elements to use.
-		 * Please take a look at res/layout/main.xml to see how the UI elements are defined.
-		 * 
-		 * R is an automatically generated class that contains pointers to statically declared
-		 * "resources" such as UI elements and strings. For example, R.layout.main refers to the
-		 * entire UI screen declared in res/layout/main.xml file. You can find other examples of R
-		 * class variables below.
-		 */
-		setContentView(R.layout.main);
+        /*
+         * Allow this Activity to use a layout file that defines what UI elements to use.
+         * Please take a look at res/layout/main.xml to see how the UI elements are defined.
+         * 
+         * R is an automatically generated class that contains pointers to statically declared
+         * "resources" such as UI elements and strings. For example, R.layout.main refers to the
+         * entire UI screen declared in res/layout/main.xml file. You can find other examples of R
+         * class variables below.
+         */
+        setContentView(R.layout.main);
 
-		/*
-		 * Calculate the port number that this AVD listens on.
-		 * It is just a hack that I came up with to get around the networking limitations of AVDs.
-		 * The explanation is provided in the PA1 spec.
-		 */
-		TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
-		String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
-		final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
+        /*
+         * Calculate the port number that this AVD listens on.
+         * It is just a hack that I came up with to get around the networking limitations of AVDs.
+         * The explanation is provided in the PA1 spec.
+         */
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        final String myPort = String.valueOf((Integer.parseInt(portStr) * 2));
 
-		try {
-			/*
-			 * Create a server socket as well as a thread (AsyncTask) that listens on the server
-			 * port.
-			 * 
-			 * AsyncTask is a simplified thread construct that Android provides. Please make sure
-			 * you know how it works by reading
-			 * http://developer.android.com/reference/android/os/AsyncTask.html
-			 */
-			ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
-			new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
-		} catch (IOException e) {
-			/*
-			 * Log is a good way to debug your code. LogCat prints out all the messages that
-			 * Log class writes.
-			 * 
-			 * Please read http://developer.android.com/tools/debugging/debugging-projects.html
-			 * and http://developer.android.com/tools/debugging/debugging-log.html
-			 * for more information on debugging.
-			 */
-			Log.e(TAG, "Can't create a ServerSocket");
-			return;
-		}
+        try {
+            /*
+             * Create a server socket as well as a thread (AsyncTask) that listens on the server
+             * port.
+             * 
+             * AsyncTask is a simplified thread construct that Android provides. Please make sure
+             * you know how it works by reading
+             * http://developer.android.com/reference/android/os/AsyncTask.html
+             */
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            /*
+             * Log is a good way to debug your code. LogCat prints out all the messages that
+             * Log class writes.
+             * 
+             * Please read http://developer.android.com/tools/debugging/debugging-projects.html
+             * and http://developer.android.com/tools/debugging/debugging-log.html
+             * for more information on debugging.
+             */
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
 
-		/*
-		 * Retrieve a pointer to the input box (EditText) defined in the layout
-		 * XML file (res/layout/main.xml).
-		 * 
-		 * This is another example of R class variables. R.id.edit_text refers to the EditText UI
-		 * element declared in res/layout/main.xml. The id of "edit_text" is given in that file by
-		 * the use of "android:id="@+id/edit_text""
-		 */
-		final EditText editText = (EditText) findViewById(R.id.edit_text);
+        /*
+         * Retrieve a pointer to the input box (EditText) defined in the layout
+         * XML file (res/layout/main.xml).
+         * 
+         * This is another example of R class variables. R.id.edit_text refers to the EditText UI
+         * element declared in res/layout/main.xml. The id of "edit_text" is given in that file by
+         * the use of "android:id="@+id/edit_text""
+         */
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
 
-		/*
-		 * Register an OnKeyListener for the input box. OnKeyListener is an event handler that
-		 * processes each key event. The purpose of the following code is to detect an enter key
-		 * press event, and create a client thread so that the client thread can send the string
-		 * in the input box over the network.
-		 */
-		editText.setOnKeyListener(new OnKeyListener() {
-			@Override
-			public boolean onKey(View v, int keyCode, KeyEvent event) {
-				if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-						(keyCode == KeyEvent.KEYCODE_ENTER)) {
-					/*
-					 * If the key is pressed (i.e., KeyEvent.ACTION_DOWN) and it is an enter key
-					 * (i.e., KeyEvent.KEYCODE_ENTER), then we display the string. Then we create
-					 * an AsyncTask that sends the string to the remote AVD.
-					 */
-					String msg = editText.getText().toString() + "\n";
-					editText.setText(""); // This is one way to reset the input box.
-					TextView localTextView = (TextView) findViewById(R.id.local_text_display);
-					localTextView.append("\t" + msg); // This is one way to display a string.
-					TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
-					remoteTextView.append("\n");
+        /*
+         * Register an OnKeyListener for the input box. OnKeyListener is an event handler that
+         * processes each key event. The purpose of the following code is to detect an enter key
+         * press event, and create a client thread so that the client thread can send the string
+         * in the input box over the network.
+         */
+        editText.setOnKeyListener(new OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    /*
+                     * If the key is pressed (i.e., KeyEvent.ACTION_DOWN) and it is an enter key
+                     * (i.e., KeyEvent.KEYCODE_ENTER), then we display the string. Then we create
+                     * an AsyncTask that sends the string to the remote AVD.
+                     */
+                    String msg = editText.getText().toString() + "\n";
+                    editText.setText(""); // This is one way to reset the input box.
+                    TextView localTextView = (TextView) findViewById(R.id.local_text_display);
+                    localTextView.append("\t" + msg); // This is one way to display a string.
+                    TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
+                    remoteTextView.append("\n");
 
-					/*
-					 * Note that the following AsyncTask uses AsyncTask.SERIAL_EXECUTOR, not
-					 * AsyncTask.THREAD_POOL_EXECUTOR as the above ServerTask does. To understand
-					 * the difference, please take a look at
-					 * http://developer.android.com/reference/android/os/AsyncTask.html
-					 */
-					new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
-					return true;
-				}
-				return false;
-			}
-		});
-	}
+                    /*
+                     * Note that the following AsyncTask uses AsyncTask.SERIAL_EXECUTOR, not
+                     * AsyncTask.THREAD_POOL_EXECUTOR as the above ServerTask does. To understand
+                     * the difference, please take a look at
+                     * http://developer.android.com/reference/android/os/AsyncTask.html
+                     */
+                    new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg, myPort);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
 
-	/***
-	 * ServerTask is an AsyncTask that should handle incoming messages. It is created by
-	 * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
-	 * 
-	 * Please make sure you understand how AsyncTask works by reading
-	 * http://developer.android.com/reference/android/os/AsyncTask.html
-	 * 
-	 * @author stevko
-	 *
-	 */
-	private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+    /***
+     * ServerTask is an AsyncTask that should handle incoming messages. It is created by
+     * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
+     * 
+     * Please make sure you understand how AsyncTask works by reading
+     * http://developer.android.com/reference/android/os/AsyncTask.html
+     * 
+     * @author stevko
+     *
+     */
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
 
-		@Override
-		protected Void doInBackground(ServerSocket... sockets) {
-
-
-
-
-			String Incoming;
-			try{
-				ServerSocket serverSocket = sockets[0];
-				while(true){
-					Socket accept= serverSocket.accept();
-					BufferedReader in = new BufferedReader(new InputStreamReader(accept.getInputStream()));
-					while((Incoming= in.readLine()) != null ){
-
-						publishProgress(Incoming);
-					}
-					accept.close();
-				}
+        @Override
+        protected Void doInBackground(ServerSocket... sockets) {
 
 
 
+            /*
+             * The following code continues to check the incoming server for any new messages and calls 
+             * onProgressUpdate as per the working of AsyncTask.  
+             */
+
+
+            String Incoming;
+            try{
+                ServerSocket serverSocket = sockets[0];
+                while(true){
+                    Socket accept= serverSocket.accept();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+                    while((Incoming= in.readLine()) != null ){
+
+                        publishProgress(Incoming);
+                    }
+                    accept.close();
+                }
 
 
 
-				/*
-				 * TODO: Fill in your server code that receives messages and passes them
-				 * to onProgressUpdate().
-				 */
-
-			} 
-			catch(Exception e){
-				Log.e(TAG, "Problem while recieving");
-			}
 
 
-			return null;
-		}
 
-		protected void onProgressUpdate(String...strings) {
-			/*
-			 * The following code displays what is received in doInBackground().
-			 */
-			String strReceived = strings[0].trim();
-			TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
-			remoteTextView.append(strReceived + "\t\n");
-			TextView localTextView = (TextView) findViewById(R.id.local_text_display);
-			localTextView.append("\n");
+                /*
+                 * TODO: Fill in your server code that receives messages and passes them
+                 * to onProgressUpdate().
+                 */
 
-			/*
-			 * The following code creates a file in the AVD's internal storage and stores a file.
-			 * 
-			 * For more information on file I/O on Android, please take a look at
-			 * http://developer.android.com/training/basics/data-storage/files.html
-			 */
+            } 
+            catch(Exception e){
+                Log.e(TAG, "Problem while recieving");
+            }
 
-			String filename = "SimpleMessengerOutput";
-			String string = strReceived + "\n";
-			FileOutputStream outputStream;
 
-			try {
-				outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
-				outputStream.write(string.getBytes());
-				outputStream.close();
-			} catch (Exception e) {
-				Log.e(TAG, "File write failed");
-			}
+            return null;
+        }
 
-			return;
-		}
-//		@Override
-//		protected void onPostExecute(Void result) {
-//			// TODO Auto-generated method stub
-//			System.out.println("ending");
-//
-//		}
-	}
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            TextView remoteTextView = (TextView) findViewById(R.id.remote_text_display);
+            remoteTextView.append(strReceived + "\t\n");
+            TextView localTextView = (TextView) findViewById(R.id.local_text_display);
+            localTextView.append("\n");
 
-	/***
-	 * ClientTask is an AsyncTask that should send a string over the network.
-	 * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
-	 * an enter key press event.
-	 * 
-	 * @author stevko
-	 *
-	 */
-	private class ClientTask extends AsyncTask<String, Void, Void> {
+            /*
+             * The following code creates a file in the AVD's internal storage and stores a file.
+             * 
+             * For more information on file I/O on Android, please take a look at
+             * http://developer.android.com/training/basics/data-storage/files.html
+             */
 
-		@Override
-		protected Void doInBackground(String... msgs) {
-			try {
-				String remotePort = REMOTE_PORT0;
-				if (msgs[1].equals(REMOTE_PORT0))
-					remotePort = REMOTE_PORT1;
+            String filename = "SimpleMessengerOutput";
+            String string = strReceived + "\n";
+            FileOutputStream outputStream;
 
-				Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
-						Integer.parseInt(remotePort));
+            try {
+                outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+                outputStream.write(string.getBytes());
+                outputStream.close();
+            } catch (Exception e) {
+                Log.e(TAG, "File write failed");
+            }
 
-				String msgToSend = msgs[0];
-				/*
-				 * TODO: Fill in your client code that sends out a message.
-				 * 
-				 */
-				//System.out.println("sending data");
-				PrintWriter out=new PrintWriter(socket.getOutputStream(), true);
-				out.print(msgToSend);
-				out.flush();
-				out.close();
-				socket.close();
-			} catch (UnknownHostException e) {
-				Log.e(TAG, "ClientTask UnknownHostException");
-			} catch (IOException e) {
-				Log.e(TAG, "ClientTask socket IOException");
-			}
+            return;
+        }
+        //		@Override
+        //		protected void onPostExecute(Void result) {
+        //			// TODO Auto-generated method stub
+        //			System.out.println("ending");
+        //
+        //		}
+    }
 
-			return null;
-		}
-	}
+    /***
+     * ClientTask is an AsyncTask that should send a string over the network.
+     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
+     * an enter key press event.
+     * 
+     * @author stevko
+     *
+     */
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            try {
+                String remotePort = REMOTE_PORT0;
+                if (msgs[1].equals(REMOTE_PORT0))
+                    remotePort = REMOTE_PORT1;
+
+                Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}),
+                        Integer.parseInt(remotePort));
+
+                String msgToSend = msgs[0];
+                /*
+                 * TODO: Fill in your client code that sends out a message.
+                 * 
+                 */
+                //System.out.println("sending data");
+                PrintWriter out=new PrintWriter(socket.getOutputStream(), true);
+                out.print(msgToSend);
+                out.flush();
+                out.close();
+                socket.close();
+            } catch (UnknownHostException e) {
+                Log.e(TAG, "ClientTask UnknownHostException");
+            } catch (IOException e) {
+                Log.e(TAG, "ClientTask socket IOException");
+            }
+
+            return null;
+        }
+    }
 }
